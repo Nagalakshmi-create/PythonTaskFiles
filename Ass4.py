b368b5563c69799a24005bf17b344d9179cca8a6
Nagalakshmi-create <99468260+Nagalakshmi-create@users.noreply.github.com>
2022-04-08 21:38:27 +0530

    Delete Ass4.py

--- Ass4.py
+++
@@ -1,8 +0,0 @@
-#create four list containig string and combine every element from each list in a single tuple
-
-list_1 = ["a", "e", "i"]
-list_2 = ["b", "f", "j"]
-list_3 = ["c", "g", "k"]
-list_4 = ["d", "h", "l"]
-
-print(list(zip(list_1, list_2, list_3, list_4)))
