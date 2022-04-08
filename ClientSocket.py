bad0cd6266427cbe5142db9c91bbeb5f65a5fa92
Nagalakshmi-create <99468260+Nagalakshmi-create@users.noreply.github.com>
2022-04-08 21:38:44 +0530

    Delete ClientSocket.py

--- ClientSocket.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-client = socket.socket()
-client.connect(('localhost', 65205))       #ip of the system in which server program is running
-
-while True:
-    msg = input("sister: ")
-    client.send(bytes(msg, 'utf-8'))
-    print(client.recv(1024).decode())
