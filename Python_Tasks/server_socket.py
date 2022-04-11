0bc60bde8dcc0e01cb7ab5d85d5cbc6612328e3b
Nagalakshmi-create <lakshmi73862@gmail.com>
2022-04-11 21:35:59 +0530

    Socket program

--- Python_Tasks/server_socket.py
+++ Python_Tasks/server_socket.py
@@ -4,25 +4,27 @@
 #server side program
 import socket
 
-server=socket.socket()   #default ipv4 and tcp
+server = socket.socket()   #default ipv4 and tcp
 HOST = ''       #to receive message
 PORT = 65205
-server.bind((HOST,PORT))
+server.bind((HOST, PORT))
 server.listen()         
 client, address = server.accept()
 print('This is client address : {}'.format(address))
+
 while True:
     print(client.recv(1024).decode())       #send data in bytes for decoding data we are using decode
-    reply=input("Naga : ")
+    reply = input("Naga : ")
     client.sendall(bytes(reply, 'utf-8'))
 
 
 #client side program
 import socket
 
-client=socket.socket()
-client.connect(('ip', 65205))#ip of the system in which server program is running
+client = socket.socket()
+client.connect(('ip', 65205))  #ip of the system in which server program is running
+
 while True:
-    msg=input("sister: ")
+    msg = input("sister: ")
     client.send(bytes(msg, 'utf-8'))
     print(client.recv(1024).decode())
