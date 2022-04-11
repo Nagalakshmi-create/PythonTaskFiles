d6b7ef9e2a5356dd8db74a91587593df79cd3ff9
Nagalakshmi-create <99468260+Nagalakshmi-create@users.noreply.github.com>
2022-04-11 19:40:43 +0530

    Update Disarium1.py

--- Python_Tasks/Disarium1.py
+++ Python_Tasks/Disarium1.py
@@ -1,9 +1,15 @@
 #Create a function that determines whether a number is a Disarium or not.
 
-def disarium(n):  #178
-
-    num_list = list(n)   #['1', '7', '8']
-    length = len(num_list)   #3
+def disarium(n): 
+    
+    '''returns whether the given number is disarium not
+        
+        parameters:
+            n (int): integer
+     '''
+    
+    num_list = list(n)   
+    length = len(num_list)   
     sum = 0
 
     for i in range(length):
