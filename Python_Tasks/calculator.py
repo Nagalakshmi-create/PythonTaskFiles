dced058deb8fc0ab31ab5d2fe6c62816dd209638
Nagalakshmi-create <99468260+Nagalakshmi-create@users.noreply.github.com>
2022-04-12 11:19:16 +0530

    Mathematical Operations

--- Python_Tasks/calculator.py
+++ Python_Tasks/calculator.py
@@ -1,11 +1,14 @@
 def add (num1, num2) :
     return num1 + num2
 
+
 def sub(num1, num2):
     return num1 - num2
 
+
 def mul(num1, num2) :
     return num1 * num2
+
 
 def div(num1, num2):
     return num1 / num2
