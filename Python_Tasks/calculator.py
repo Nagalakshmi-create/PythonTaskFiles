febe82d9066797e515ae907549b493de77029c83
Nagalakshmi-create <99468260+Nagalakshmi-create@users.noreply.github.com>
2022-04-12 20:39:34 +0530

    Mathematical Operations

--- Python_Tasks/calculator.py
+++ Python_Tasks/calculator.py
@@ -1,14 +1,57 @@
 def add (num1, num2) :
+    
+    '''
+        Parameters:
+            num1(int) : Any integer
+            num2(int) : Any integer
+            
+        Returns:
+            Returns the addition of num1 and num2
+    '''
+    
     return num1 + num2
 
 
 def sub(num1, num2):
+    
+     
+    '''
+        Parameters:
+            num1(int) : Any integer
+            num2(int) : Any integer
+            
+        Returns:
+            Returns the subtraction of num1 and num2
+    '''
+    
     return num1 - num2
 
 
 def mul(num1, num2) :
+    
+     
+    '''
+        Parameters:
+            num1(int) : Any integer
+            num2(int) : Any integer
+            
+        Returns:
+            Returns the multiplication of num1 and num2
+    '''
+    
     return num1 * num2
 
 
 def div(num1, num2):
+    
+     
+    '''
+        Parameters:
+            num1(int) : Any integer
+            num2(int) : Any integer
+            
+        Returns:
+            Returns the division of num1 and num2
+    '''
+    
     return num1 / num2
