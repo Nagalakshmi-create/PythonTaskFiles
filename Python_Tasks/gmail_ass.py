2b50d7b5a14e7d1d268f97ff27e1aac39b6d1ca8
Nagalakshmi-create <99468260+Nagalakshmi-create@users.noreply.github.com>
2022-04-12 20:26:11 +0530

    Assignment

---
+++ Python_Tasks/gmail_ass.py
@@ -0,0 +1,33 @@
+#send mail to me through python script
+#keep cc to other two if naga is writing script she will keep radha and shivani in cc
+
+import smtplib
+
+sent_from= 'lakshmi73862@gmail.com'
+gmail_password = 'uhfpojsksibkvwwz'
+
+#to = ['vibha.rawan@neosoftmail.com']
+to=['lakshmi73862@gmail.com']
+
+#cc = ["grandheraj68@gmail.com", "radha.maredi@gmail.com"]
+cc = ["grandheraj68@gmail.com"]
+#bcc = ["arun.nadar@neosoftmail.com", "rohan.dhere@neosoftmail.com"]
+bcc=["radha.maredi@gmail.com"]
+
+subject = 'Introduction'
+body = 'Hello, Iam Nagalakshmi. This email is sent through Python script.'
+
+email_text = """\
+From: %s
+To: %s
+Subject: %s
+cc: %s
+bcc: %s
+%s
+""" % (sent_from, ", ".join(to), subject,", ".join(cc), ", ".join(bcc), body)
+
+smtp_server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+smtp_server.login(sent_from, gmail_password)
+smtp_server.sendmail(sent_from, to + cc + bcc, email_text)
+smtp_server.close()
+print ("Email sent successfully!")
