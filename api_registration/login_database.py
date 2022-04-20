5c1936d58e7ca63c614ca18dfe6f64d490d37657
Nagalakshmi-create <lakshmi73862@gmail.com>
2022-04-20 15:58:59 +0530

    login and registration page using sqlite3

--- api_registration/login_database.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-
-conn = sqlite3.connect('login_db.db')
-print("Connected")
-conn.execute("""CREATE TABLE user_details(user_name VARCHAR(100), email VARCHAR(200), 
-                                          phone_no INT, password VARCHAR(100))""")
-print("Table is created")
-conn.close()
