fa906a7a4f7fe47e5feba554667e14a3457d833a
Nagalakshmi-create <99468260+Nagalakshmi-create@users.noreply.github.com>
2022-05-24 12:05:48 +0530

    Python Assignments

--- api_registration/database.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('database.db')
-print('opened')
-conn.execute(""" CREATE TABLE details(name VARCHAR(255) ,email CHAR(25) ,contact varchar(255),password varchar(255))""")
-print('Table created')
-conn.close()
