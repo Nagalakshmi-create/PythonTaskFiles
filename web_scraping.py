2530cd9ca2b53050ccee59e3c54ddf63d3639ad2
Nagalakshmi-create <99468260+Nagalakshmi-create@users.noreply.github.com>
2022-05-30 19:01:07 +0530

    Web scraping on Patanjali website

--- web_scraping.py
+++ web_scraping.py
@@ -8,27 +8,18 @@ from urllib.request import urlopen
 import pandas as pd
 
 url = "https://www.patanjaliayurved.net/category/natural-food-products/2"
-
 response = requests.get(url)
 # The get() method sends a GET request to the specified url
 print(response.status_code)
-
 soup = BeautifulSoup(response.content, 'lxml')
 # lxml is a Python library which allows for easy handling of XML and HTML files
-
 divs = soup.find_all('div', {'class': "col-md-9 col-sm-9 col-xs-12 filter-sidebar-box-right"})
-
 names = divs[0].find_all('a', {'class': "product-name tool_tip"})
-
 product_names = [name.string.strip() for name in names]
-
 prices = divs[0].find_all('p', {'class': "product-price"})
-
 product_prices = [price.string.strip() for price in prices]
-
 weights = divs[0].find_all('div', {'class': "block-name list-page-blog-name"})
 product_weights = [weight.text.strip() for weight in weights]
-
 weight_list = []
 for i in range(len(product_weights)):
     a = product_weights[i].find("\n")
@@ -37,20 +28,16 @@ for i in range(len(product_weights)):
     d = c[a+110:]
     d = re.sub("\xa0", " ", d)
     weight_list.append(d.strip())
-
-
+    
 image_links = []
 htmldata = urlopen('https://www.patanjaliayurved.net/category/natural-food-products/2')
 soups = BeautifulSoup(htmldata, 'html.parser')
-images = soups.find_all('img', {'class': 'img-responsive'})
-  
+images = soups.find_all('img', {'class': 'img-responsive'})  
 for item in images:
     image_links.append(item['src'])
-
-
+    
 product_links = []
 links = soups.find_all('a', {'class': 'figure-href'})
-  
 for link in links:
     product_links.append(link['href'])
 
@@ -61,5 +48,4 @@ products['Product Type'] = "Natural Food Products"
 products['Weight'] = weight_list
 products['Price'] = product_prices
 products['Products Links'] = product_links
-
 print(products)
